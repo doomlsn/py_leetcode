abee1beddf8e9dafa370d9d84e04f29935b77b99
doomlsn <doomlsn@gmail.com>
2016-05-06 11:42:24 -0700

    alpha_version_5_6
    This version can solve the problem but run time exceed requirement.

--- python_source/LongestSubstringWithoutRepeatingCharacters.py
+++ python_source/LongestSubstringWithoutRepeatingCharacters.py
@@ -6,18 +6,30 @@ class Solution(object):
         """
         input_string = s
         index_start = 0
-        index = 0
+        max_index = 0
+        max_length = 0
         #index = index_start
         length = 0
-        current_length = length
-        substring = []
-        current_substring = substring
+        current_length = 0
+        substring = ''
+        current_substring = ''
         flag = 0
-        for character in input_string:
-            index += 1
-            for element in substring:
-                if element == character:
+        while len(input_string)>0:
+            for character in input_string:
+                if current_substring.find(character) != -1:
                     flag = 1
-            if flag == 0:
-                substring += character
-                length += 1
+                if flag == 0:
+                    current_substring += character
+                    current_length += 1
+                if flag == 1:
+                    length = current_length
+                    current_length = 0
+                    substring = current_substring
+                    current_substring = ''
+                    flag = 0
+                    if max_length < length:
+                        max_length = length
+                        #max_index = i
+                    break
+            input_string=input_string[1:]
+        return max_length
